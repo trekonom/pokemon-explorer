66119c93c63f8be0ade5bf5db1b7524c64858fa2
Stefan Moog <moogs@gmx.de>
2026-03-22 10:30:09 +0100

    Initial Commit

---
+++ src/PokemonCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css"; // reuse existing stylesheet for simplicity
+
+export default function PokemonCard({ pokemon }) {
+  const { id, name, type, hp, attack } = pokemon;
+  const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+  return (
+    <div className="pokemon-card">
+      <img src={spriteUrl} alt={name} className="pokemon-image" />
+      <h2>{name}</h2>
+      <p>Type: {type}</p>
+      <p>HP: {hp}</p>
+      <p>Attack: {attack}</p>
+    </div>
+  );
+}
